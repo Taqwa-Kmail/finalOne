def3d2a5d6fb7815e759082f24da9ae2aa2e133d
Taqwa-Kmail <taqwa.ykmail@gmail.com>
2023-06-24 18:36:12 +0300

    from samba

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#created by taqwa
+cd /srv/samba/taqwa_201911773
+git add .
+git commit -m "from samba"
+git push origin master >> /var/log/taqwa.log 2>&1
+if [ $? -eq 0 ]
+then 
+ echo "success at $(date)" >> /var/log/taqwa.log
+else
+ echo "faild at $(date)" >> /var/log/taqwa.log
+fi 
+
